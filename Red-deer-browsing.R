3844ea07fc7a7afc5ec38668b4dd566d5227c5f3
marielaj <marielaj@stud.ntnu.no>
2019-01-04 10:56:10 +0100

    Test2

---
+++ Red-deer-browsing.R
@@ -0,0 +1,9 @@
+#Master prosjekt - Red deer browsing 
+
+#Importerer Species_2009-2018 dataset 
+
+library(readxl)
+Species_2009_2018_2_ <- read_excel("~/Master/Data/Species 2009_2018 (2).xlsx")
+View(Species_2009_2018_2_)
+
+View(Species_2009_2018_2_)
